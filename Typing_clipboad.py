d0ff5f45b2d3bd02d8f5df65f7311269879d8b97
Lasitha Jayawardana <65305702+Lasitha-Jayawardana@users.noreply.github.com>
2021-02-09 01:11:56 +0530

    Add automatically typing

--- Typing_clipboad.py
+++ Typing_clipboad.py
@@ -3,10 +3,6 @@ import time
 import pyperclip
 
 str = ""
-keypress = False
-
-
-
 
 while True:
     time.sleep(0.1)
@@ -15,30 +11,30 @@ while True:
         
         str =pyperclip.paste()
         
-
         pyperclip.copy(str[0])
-
+        print(str[0])
+        str = str[1:]
+        
         while True:
                
-            time.sleep(0.01)
+            time.sleep(0.1)
                     
-            if keypress and not keyboard.is_pressed("v"):
-                    if (str):
-                        pyperclip.copy(str[0])
-                        print(str[0])
-                        str = str[1:]
-                    else:
-                        print("stop")
-                        break
+            
+            if (str):
+                keyboard.press("ctrl")
+                keyboard.press_and_release("v")
+                keyboard.release("ctrl")
+                pyperclip.copy(str[0])
+                print(str[0])
+                str = str[1:]
+            else:
+                keyboard.press("ctrl")
+                keyboard.press_and_release("v")
+                keyboard.release("ctrl")
+                print("stop")
+                
+                break
                         
-                    #beak out of while loop?
-                    keypress = False
-                    #break
-            elif keyboard.is_pressed("v") and not keypress:
-                    keypress = True
-                    
-            
-            
-            elif keyboard.is_pressed("*"):
+            if keyboard.is_pressed("*"):
                 print("stop")
                 break
